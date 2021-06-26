6157c28c355956eb381f22b542a0d7d9a0b3f2ee
Sanajit Jana <sanajitjana1402@gmail.com>
2021-06-26 14:09:36 +0530

    Rrename Navigation to Hearder

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,4 +1,4 @@
-export { default as Navigation } from "./Navigation";
+export { default as Navigation } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as About } from "./About.jsx";
