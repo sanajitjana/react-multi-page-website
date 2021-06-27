af38c8dea934458fb716d372c753b2b04e906ffc
Sanajt Jana <sanajitjana1402@gmail.com>
2021-06-27 16:56:32 +0530

    rename jsx to js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-function Contact() {
+function About() {
   return (
-    <div className="contact">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+    <div className="about">
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-7">
             <img
-              class="img-fluid rounded mb-4 mb-lg-0"
+              className="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
               alt=""
             />
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Contact</h1>
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">About</h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
@@ -27,4 +27,4 @@ function Contact() {
   );
 }
 
-export default Contact;
+export default About;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,5 @@
 export { default as Navigation } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
-export { default as About } from "./About.jsx";
+export { default as About } from "./About";
 export { default as Contact } from "./Contact";
