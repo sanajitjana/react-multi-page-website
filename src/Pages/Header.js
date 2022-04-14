f6aa765e1dc74f3e40c663f91fc7cccb738f6126
Sanajit Jana <dev.sanajitjana@gmail.com>
2022-04-15 00:11:02 +0530

    update url

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -6,14 +6,20 @@ function Navigation(props) {
     <div className="navigation">
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="container">
-          <Link className="navbar-brand" to="/">
+          <Link
+            className="navbar-brand"
+            to="https://sanajitjana.github.io/react-multi-page-website/"
+          >
             React Multi-Page Website
           </Link>
           <div>
             <ul className="navbar-nav ml-auto">
               <li
                 className={`nav-item  ${
-                  props.location.pathname === "/" ? "active" : ""
+                  props.location.pathname ===
+                  "https://sanajitjana.github.io/react-multi-page-website/home"
+                    ? "active"
+                    : ""
                 }`}
               >
                 <Link className="nav-link" to="/">
@@ -23,19 +29,31 @@ function Navigation(props) {
               </li>
               <li
                 className={`nav-item  ${
-                  props.location.pathname === "/about" ? "active" : ""
+                  props.location.pathname ===
+                  "https://sanajitjana.github.io/react-multi-page-website/about"
+                    ? "active"
+                    : ""
                 }`}
               >
-                <Link className="nav-link" to="/about">
+                <Link
+                  className="nav-link"
+                  to="https://sanajitjana.github.io/react-multi-page-website/about"
+                >
                   About
                 </Link>
               </li>
               <li
                 className={`nav-item  ${
-                  props.location.pathname === "/contact" ? "active" : ""
+                  props.location.pathname ===
+                  "https://sanajitjana.github.io/react-multi-page-website/contact"
+                    ? "active"
+                    : ""
                 }`}
               >
-                <Link className="nav-link" to="/contact">
+                <Link
+                  className="nav-link"
+                  to="https://sanajitjana.github.io/react-multi-page-website/contact"
+                >
                   Contact
                 </Link>
               </li>
